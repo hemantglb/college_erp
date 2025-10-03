821a20b25e47edb0835a475aa8b70410be95ffc4
HEMANT KUMAR <hemantfsu@gmail.com>
2025-10-03 20:46:52 +0530

    changes in vite config base url

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/college_erp/", // <-- important for GitHub Pages
+  base: "/", // <-- important for GitHub Pages
 });
